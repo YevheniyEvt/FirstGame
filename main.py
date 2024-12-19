3432657f4798f0aaa18802662615077e6b49eba8
Yevheniy <Genya421@gmail.com>
2024-12-19 18:51:43 +0200

    make game field

---
+++ main.py
@@ -0,0 +1,88 @@
+import pygame
+pygame.font.init()
+
+SIZE = 80
+WIDTH, HEIGHT = SIZE*10, SIZE*10
+
+GAME_FIELD_WIDTH, GAME_FIELD_HEIGHT = WIDTH - (SIZE * (WIDTH/SIZE)*0.3), HEIGHT
+PRE_GAME_WIDTH, PRE_GAME_HEIGHT = WIDTH-GAME_FIELD_WIDTH, HEIGHT
+
+GAME_FIELD_COLOR = (71,70,68)
+PRE_GAME_FIELD_COLOR = (222, 209, 175)
+
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+FPS = 60
+
+class Line:
+    size = SIZE
+
+    def __init__(self, x, y, color, width):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.width = width
+
+    def draw_vertical_line(self, window):
+        pygame.draw.line(window, self.color,
+                         (self.x + self.size, self.y),
+                         (self.x +self.size, self.y + HEIGHT),
+                         self.width)
+
+    def draw_horizontal_line(self, window):
+        pygame.draw.line(window, self.color,
+                         (self.x, self.y + self.size),
+                         (self.x + GAME_FIELD_WIDTH, self.y + self.size),
+                         self.width)
+
+
+    def change_x(self):
+        self.x += self.size
+
+
+
+
+def draw_field(width_field, height_field):
+
+    colum_number = int(GAME_FIELD_WIDTH/SIZE)
+    row_number = int(GAME_FIELD_HEIGHT/SIZE)
+    line = Line(0,0,(102, 99, 89), 2)
+
+    for number in range(colum_number):
+        line.draw_vertical_line(WINDOW)
+        line.change_x()
+    line.x = 0
+
+    for number_2 in range(row_number):
+        line.draw_horizontal_line(WINDOW)
+        line.y += SIZE
+
+
+def draw(game_field, pre_game_field):
+    ...
+def main():
+    clock = pygame.time.Clock()
+    run = True
+
+    game_field = pygame.Rect(0,0,GAME_FIELD_WIDTH,GAME_FIELD_HEIGHT)
+    pre_game_field = pygame.Rect(GAME_FIELD_WIDTH, 0, PRE_GAME_WIDTH,PRE_GAME_HEIGHT)
+
+    while run:
+        clock.tick(FPS)
+        event = pygame.event.poll()
+        if event.type == pygame.QUIT:
+            run = False
+            break
+
+
+
+        draw(game_field, pre_game_field)
+        pygame.draw.rect(WINDOW, GAME_FIELD_COLOR, game_field)
+        pygame.draw.rect(WINDOW, PRE_GAME_FIELD_COLOR, pre_game_field)
+        draw_field(WIDTH, HEIGHT)
+
+        pygame.display.update()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
